233691714e8dfec68db8e28bf544f584f8f5b95d
Hldka <116027001+Hldka@users.noreply.github.com>
2022-12-16 18:56:31 +0100

    first commit

---
+++ src/AdvJavaPractice/enums/User.java
@@ -0,0 +1,35 @@
+package AdvJavaPractice.enums;
+
+public class User {
+    public User(String name, Role role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    private String name;
+    private Role role;
+}
